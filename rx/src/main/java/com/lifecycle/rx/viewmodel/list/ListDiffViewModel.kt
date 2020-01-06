2d93bb2f3de407e90fede0911efb9e72b40b5418
Arvin <new1033144294@qq.com>
2020-01-06 17:48:54 +0800

    add rx module

--- rx/src/main/java/com/lifecycle/rx/viewmodel/list/ListDiffViewModel.kt
+++ rx/src/main/java/com/lifecycle/rx/viewmodel/list/ListDiffViewModel.kt
@@ -1,24 +1,32 @@
 package com.lifecycle.rx.viewmodel.list
 
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListUpdateCallback
+import com.lifecycle.binding.adapter.recycler.DiffUtilCallback
 import com.lifecycle.binding.inter.inflate.Diff
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 open class ListDiffViewModel<E : Diff> : ListViewModel<E>() {
+    var d:Disposable? = null
     override fun refreshList(es: List<E>, position: Int): Boolean {
         enable.value = false
-//        diff = CoroutineScope(Dispatchers.Default).launch {
-//            val result = DiffUtil.calculateDiff(DiffUtilCallback(adapter.adapterList, es))
-//            launch(Dispatchers.Main) {
-//                adapter.adapterList.clear()
-//                adapter.adapterList.addAll(es)
-//                result.dispatchUpdatesTo(adapter as ListUpdateCallback)
-//                enable.value = true
-//            }
-//        }
+        d = Observable.just(DiffUtil.calculateDiff(DiffUtilCallback(adapter.adapterList, es)))
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                adapter.adapterList.clear()
+                adapter.adapterList.addAll(es)
+                it.dispatchUpdatesTo(adapter as ListUpdateCallback)
+                enable.value = true
+            }
         return true
     }
 
     override fun onCleared() {
         super.onCleared()
-//        diff?.cancel()
+        d?.dispose()
     }
 }
