5dfb7eab0c63ebaf0a031c32da0b5d403b460541
arvin <1033144294@qq.com>
2020-02-25 10:11:01 +0800

    remove rtc project

--- app/src/main/java/com/lifecycle/demo/inject/data/Api.kt
+++ app/src/main/java/com/lifecycle/demo/inject/data/Api.kt
@@ -1,16 +1,13 @@
 package com.lifecycle.demo.inject.data
 
 import android.content.Context
-import android.view.View
 import com.lifecycle.demo.base.util.restful
 import com.lifecycle.demo.inject.data.database.DatabaseApi
 import com.lifecycle.demo.inject.data.map.MapApi
 import com.lifecycle.demo.inject.data.net.NetApi
 import com.lifecycle.demo.inject.data.net.SignParams
-import com.lifecycle.demo.inject.data.net.bean.TokenBean
 import com.lifecycle.demo.inject.data.oss.OssApi
 import com.lifecycle.demo.inject.data.preference.PreferenceApi
-import io.reactivex.Single
 
 class Api(
     private val application: Context,
